39529ebf5457031edb78c02c7c3ddd40179bc7db
Henrique Ferreira <62857389+henferreirapro@users.noreply.github.com>
2022-10-03 20:22:08 -0300

    Update Function getUsers

--- main.js
+++ main.js
@@ -1,3 +1,8 @@
 const url = "http://localhost:5500/api"
 
 function getUsers() {
+  fetch(url)
+    .then(response => response.json())
+    .then(data => renderApiResult.textContent = JSON.stringify(data))
+    .catch(e => console.error(e))
+}
